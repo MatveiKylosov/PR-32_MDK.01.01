045c9beef98e870693031ccc4b85b8198390ea86
MatveiKylosov <matveikyl@mail.ru>
2024-03-18 00:12:24 +0500

    Разработанный класс подключения. Kylosov

---
+++ Classes/DBConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinylRecordsApplication.Classes
+{
+    public class DBConnection
+    {
+        public static DataTable Connection(string SQL)
+        {
+            DataTable dataTable = new DataTable("Datatable");
+            SqlConnection sqlConnection = new SqlConnection("server=127.0.0.1:Trusted_Connection=No;DataBase=VinylRecords;User=SA; PWD=Asdfg123");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = SQL;
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
+            sqlDataAdapter.Fill(dataTable);
+        }
+    }
+}
