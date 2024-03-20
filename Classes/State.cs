9d43cf85d44b55dbfc488aadce889c53680f7b87
MatveiKylosov <matveikyl@mail.ru>
2024-03-20 20:12:39 +0500

    Разработанная страница добавления и изменения состояний. Kylosov

--- Classes/State.cs
+++ Classes/State.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace VinylRecordsApplication.Classes
 {
-    internal class State
+    public class State
     {
         public int Id { get; set; }
         public string Name { get; set; }
